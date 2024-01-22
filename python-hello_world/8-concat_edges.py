5d694c43a4744a0f4266b5e9c29b1070ee23b6e0
MDrenova <mateodrenova@gmail.com>
2024-01-22 03:33:48 -0800

    Concatenate string

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+word = word[39:66] + " " + word[111:116] + word[0:6]
+print(str)
