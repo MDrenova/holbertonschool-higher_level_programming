7ddf47e035359ecbfebcfd1278f41ca7362a680e
MDrenova <mateodrenova@gmail.com>
2024-01-22 06:18:14 -0800

    Change condition varaible

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 lDigit = abs(number) % 10 * (-1 if number < 0 else 1)
-if (lastDigit == 0):
+if (lDigit == 0):
     print(f"Last digit of {number} is {lDigit} and is 0")
-elif (number > 5):
+elif (lDigit > 5):
     print(f"Last digit of {number} is {lDigit} and is greater than 5")
 else:
     print(f"Last digit of {number} is {lDigit} and is less than 6 and not 0")
