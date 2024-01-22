9e5daee44eafccb6ec4e9129c7981fdfa3e45b22
MDrenova <mateodrenova@gmail.com>
2024-01-22 03:59:18 -0800

    Update index to print string

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-word = word[39:66] + " " + word[111:116] + word[0:6]
+word = word[39:67] + word[107:112] + word[0:6]
 print(str)
