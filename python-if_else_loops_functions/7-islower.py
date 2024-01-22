f444a3b56f43be0d2c4d97b41cbafcf57adfcc20
MDrenova <mateodrenova@gmail.com>
2024-01-22 15:51:43 -0800

    Add a loop to check all the string

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if((ord(c) >= 97) and (ord(c) <= 122)):
-        return True
+    for char in c:
+        if((ord(char) >= 97) and (ord(char) <= 122)):
+            return True
     return False
