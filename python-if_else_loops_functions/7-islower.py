6b4e339f2877fb3ad3db03221373af9ddfadf473
MDrenova <mateodrenova@gmail.com>
2024-01-22 15:37:49 -0800

    Update condition start from letter a = 97

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    if((ord(c) >= 98) and (ord(c) <= 122)):
-        print(c, " is lower")
-    else:
-        print(c, " is upper")
+    if((ord(c) >= 97) and (ord(c) <= 122)):
+        return True
+    return False
