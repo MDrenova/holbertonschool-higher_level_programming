c60a006691ab9a4cd90764b88a56507bc7e8ff35
MDrenova <mateodrenova@gmail.com>
2024-01-22 04:01:44 -0800

    Update variable name

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-word = word[39:67] + word[107:112] + word[0:6]
+str = str[39:67] + str[107:112] + str[0:6]
 print(str)
