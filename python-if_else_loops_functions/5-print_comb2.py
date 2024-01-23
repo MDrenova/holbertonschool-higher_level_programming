278f053e5b81165dad20e99dd2717e955333849f
MDrenova <mateodrenova@gmail.com>
2024-01-23 14:15:00 -0800

    Add a comment that explain a diffrent way to print

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for nr in range(0, 100):
     if (nr < 99):
+        # or it can be print("{:02d}, ".format(num), end="")
+        # more simple
         print("0{}, ".format(nr) if nr < 10 else "{}, ".format(nr), end="")
     else:
         print("{}".format(nr))
