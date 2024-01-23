abe597f49cf79da379f8a828d586d0dbb1c8004a
MDrenova <mateodrenova@gmail.com>
2024-01-23 03:39:51 -0800

    Remove import modules

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-import 7-islower.py
-
-
 def uppercase(str):
     strCpy = ''
     for char in str:
-        if islower(char):
+        if 'a' <= char <= 'z':
             strCpy += chr(ord(char) - 32)
         else:
             strCpy += char
