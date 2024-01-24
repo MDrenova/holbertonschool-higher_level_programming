2dc22b2d569aa56aa966133297c43b38cfb18796
MDrenova <mateodrenova@gmail.com>
2024-01-24 04:05:37 -0800

    Update python style

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         print("{} arguments:".format(len(argv) - 1))
-        for i in range (1, (len(argv))):
+        for i in range(1, (len(argv))):
             print("{}: {}".format(i, argv[i]))
