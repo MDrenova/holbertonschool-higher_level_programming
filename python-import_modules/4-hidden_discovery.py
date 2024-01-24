d31b001e0cb3fa4a84a5d13bf73765a24af12fbc
MDrenova <mateodrenova@gmail.com>
2024-01-24 05:09:32 -0800

    Print some words from importwed modules

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    name_list = dir(hidden_4)
+    for name in name_list:
+        if not name.startswith("__"):
+            print("{}".format(name))
