13dd87ee4fbeab01d144be56af97469439ca1c47
MDrenova <mateodrenova@gmail.com>
2024-01-26 13:27:06 -0800

    Changing the condition when to copy

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -2,8 +2,6 @@
 def remove_char_at(str, n):
     str_cpy = ""
     for i in range(0, len(str)):
-        # or if i != n than copy
-        if i == n:
-            i += 1
-        str_cpy += (str[i])
+        if i != n:
+            str_cpy += (str[i])
     return str_cpy
