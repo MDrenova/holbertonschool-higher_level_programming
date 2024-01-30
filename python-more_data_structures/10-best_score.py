cd5d86c138472ef57dd7c50e6b5ea50c2ccc49b1
MDrenova <mateodrenova@gmail.com>
2024-01-30 12:44:18 +0100

    Update conditions

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if not a_dictionary:
         # max_value = " "
         # return (max_value.join(key for key, value in a_dictionary.items()
         # if value == max(a_dictionary.values())))
-        return max(a_dictionary, key=a_dictionary.get)
-    return None
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
 
 
 a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 16, 'Molly': 16, 'Adam': 10}
