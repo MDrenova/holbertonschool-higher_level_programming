f8df2da58cf410f67bf0f9154d5ddc7f667fd879
MDrenova <mateodrenova@gmail.com>
2024-01-30 12:46:26 +0100

    Remove the call of function

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is not None:
         # max_value = " "
         # return (max_value.join(key for key, value in a_dictionary.items()
         # if value == max(a_dictionary.values())))
-        return None
-    return max(a_dictionary, key=a_dictionary.get)
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 16, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
