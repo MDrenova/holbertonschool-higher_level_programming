f3a59165243ca837f98776c6a28f41466309edb4
MDrenova <mateodrenova@gmail.com>
2024-01-31 12:34:20 +0100

    Update python code style

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a class Square that defines a square by: 
+"""Write a class Square that defines a square by:
 Private instance attribute: size
 Instantiation with size (no type/value verification)
 You are not allowed to import any module
