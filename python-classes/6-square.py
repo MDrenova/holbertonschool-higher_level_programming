f3d44356fbcf287be268f81524e82e195c9d4d6e
MDrenova <mateodrenova@gmail.com>
2024-01-31 13:08:56 -0800

    Create a class square that prints a square and with a position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -17,5 +17,40 @@ class Square:
     Args:
     size: Size of square
     """
-    def __init__(self, size=0)
-        
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for y_coordinates in range(self.__position[1]):
+            print()
+        for height in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size, end="")
+            print()
