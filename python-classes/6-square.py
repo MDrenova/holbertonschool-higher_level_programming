0e492f884c99675eaa904a39215078a3b365d9bb
MDrenova <mateodrenova@gmail.com>
2024-01-31 13:26:13 -0800

    Update condition for value in position property setter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -39,7 +39,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple):
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif not all(isinstance(item, int) and item >= 0 for item in value):
             raise TypeError("position must be a tuple of 2 positive integers")
