58a3c446933e940331423e1f89f3879f9bcfe09e
MDrenova <mateodrenova@gmail.com>
2024-02-01 14:55:27 +0100

    Create a function that returns a matrix that is quotient

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Divide a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+        Parameters:
+        matrix (int, float): matrix.
+        div (int, float): the divisor of the matrix.
+
+        Returns:
+        float: The quotient of the matrix.
+
+        Example:
+        add(2, 3)
+        5
+    """
+    if not all(isinstance(col, (int, float)) for row in matrix for col in row):
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
+    size_row = len(matrix[0])
+    for row in matrix:
+        if size_row != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = matrix
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            new_matrix[row][col] = matrix[row][col] / div
+            new_matrix[row][col] = round(new_matrix[row][col], 2)
+    return new_matrix
