170d96cbd769c149cc5328c50a236bb06b23281b
MDrenova <mateodrenova@gmail.com>
2024-02-02 12:56:56 +0100

    Update condition inside loop to check if matrix is list of lists

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,13 +25,13 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     new_matrix = []
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists)"
-                        " of integers/floats")
     for row in matrix:
         new_row = []
         if size_row != len(row):
             raise TypeError("Each row of the matrix must have the same size")
+        if not isinstance(matrix, list):
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            " of integers/floats")
         for col in row:
             if not isinstance(col, (int, float)) and not isinstance(row, list):
                 raise TypeError("matrix must be a matrix (list of lists)"
