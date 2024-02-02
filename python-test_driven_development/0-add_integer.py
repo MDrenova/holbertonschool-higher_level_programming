01d00db28accd386e740ce4a3a97d07620b00fa5
MDrenova <mateodrenova@gmail.com>
2024-02-02 12:08:01 +0100

    Create a func that return the sum of 2 int

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Adds two numbers together.
+"""
+def add_integer(a, b=98):
+    """
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+    Returns:
+    int: The sum of x and y.
+    Example:
+    add(2, 3)
+    5
+    """ 
+    if not all(isinstance(i, (int, float)) for i in (a, b)):
+        raise TypeError("a must be an integer or b must be an integer")
+    else:
+        return int(a + b)
