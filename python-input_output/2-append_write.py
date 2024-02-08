db2017e28494178cb03cd4feeec18b2fe83666e8
MDrenova <mateodrenova@gmail.com>
2024-02-08 13:49:17 +0100

    Create: read file and append

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -7,6 +7,3 @@ def append_write(filename="", text=""):
     with open(filename, 'a', encoding="utf-8") as file:
         file.write(text)
     return len(text)
-
-nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
-print(nb_characters_added)
