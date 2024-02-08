a301b5543c22d3330225f35d2523a97a0d3c4ff1
MDrenova <mateodrenova@gmail.com>
2024-02-08 11:08:27 +0100

    Update comments

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """class BaseGeometry.
 Public instance method: def area(self):
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
