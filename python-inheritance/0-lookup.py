564eb26ad5453eef29ac2f82993346e7c7aaa4b8
MDrenova <mateodrenova@gmail.com>
2024-02-08 10:49:46 +0100

    Update code style python

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a function that returns the list of available attributes and methods of an object:
+"""Write a function that returns the list of available attributes
+    and methods of an object:
 """
 
 
