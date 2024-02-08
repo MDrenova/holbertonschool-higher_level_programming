243e87e7c8d510fda16fb8b99c316f37cb738166
MDrenova <mateodrenova@gmail.com>
2024-02-08 13:03:57 +0100

    Create: function to read file and to print to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''a function that reads a file'''
+
+
+def read_file(filename=""):
+    '''reads a file'''
+    with open(filename, 'r', encoding="utf-8") as file:
+        print(file.read(), end="")
+
+read_file("1-my_list.txt")
