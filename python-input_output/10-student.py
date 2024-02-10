bf3b776c0aa38959065c476fada1a2b21542aeca
root <mateodrenova@gmail.com>
2024-02-10 20:09:08 +0000

    Upadet: code style

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,10 +13,8 @@ class Student:
     def to_json(self, attrs=None):
         if attrs is None:
             return self.__dict__
-
         new_dict = {}
-
         for key in attrs:
             if key in self.__dict__:
-                new_dict_dict[key] = self.__dict__[key]
+                new_dict[key] = self.__dict__[key]
         return new_dict
