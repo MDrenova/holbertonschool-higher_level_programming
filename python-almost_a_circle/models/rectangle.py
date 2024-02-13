8e70cde2d7df275971efb846ec8c260e75c7dbcd
MDrenova <mateodrenova@gmail.com>
2024-02-13 15:14:24 +0100

    Update: update method remove conditions

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,12 +79,10 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         '''Update class that assigns an argument'''
         arg_list = ["id", "width", "height", "x", "y"]
-        if len(args) > 0:
-            for arg_name, arg_value in zip(arg_list, args):
-                setattr(self, arg_name, arg_value)
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+        for arg_name, arg_value in zip(arg_list, args):
+            setattr(self, arg_name, arg_value)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def to_dictionary(self):
         '''dictionary'''
