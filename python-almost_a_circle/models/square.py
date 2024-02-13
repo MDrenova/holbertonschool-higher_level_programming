a892cd6b527c91e996a2000cc5158a8d050915ad
MDrenova <mateodrenova@gmail.com>
2024-02-13 14:04:50 +0100

    Create: method geter and setter for size property

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,6 +9,19 @@ class Square(Rectangle):
         '''Constructor'''
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
         '''Overwrite str square'''
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
