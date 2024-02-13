f9dfc8dc9585974726b77447d2c25a3cca7f6468
MDrenova <mateodrenova@gmail.com>
2024-02-13 13:56:56 +0100

    Update: super method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     '''class Rectangle'''
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor'''
-        super().__init__(id, size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         '''Overwrite str square'''
