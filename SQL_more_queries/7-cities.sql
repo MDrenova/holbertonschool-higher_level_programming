4b5ea8b1a5707aa9bd57eaf6ed8a67fb3a6b9484
MDrenova <mateodrenova@gmail.com>
2024-03-19 14:35:59 +0100

    Update: mysql style

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-    id int NOT NULL AUTO_INCREMENT,
-    state_id int NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL,
     name varchar(256) NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES states(state_id)
 );
