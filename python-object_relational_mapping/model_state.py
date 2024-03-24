6dcfcdf47d68ee144d3cc5ef801d9156fab96c87
MDrenova <mateodrenova@gmail.com>
2024-03-24 22:01:11 +0100

    Create: a class that difines table satate

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+# Define a SQLAlchemy model class called "State"
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Represents a state with attributes id and name.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
