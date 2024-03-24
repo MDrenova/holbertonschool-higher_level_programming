bfb6611ed4685c75ea8fb44bd1a406d4a9e564bd
MDrenova <mateodrenova@gmail.com>
2024-03-24 21:13:02 +0100

    Create: rewrite the exc 2 with safe MySQL injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Write a script that lists all states starting with N
+from the database hbtn_0e_0_usa
+'''
+
+import MySQLdb
+from sys import argv
+
+# Create a connection to the database
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",  # replace with your host name
+        port=3306,  # add port
+        user=argv[1],  # replace with your username
+        passwd=argv[2],  # replace with your password
+        database=argv[3]  # replace with your database name
+    )
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute a query
+    cur.execute("SELECT * FROM states WHERE name ORDER BY id", (argv[4],))
+
+    # Fetch all the rows
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    # Close the connection
+    cur.close()
+    db.close()
